9fa417dea9b2650a6ef2142a26ec7f7d106c88fc
vule <vule5726@gmail.com>
2021-12-05 02:38:23 +0200

    update login

---
+++ employee-vue-app/src/views/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="p-d-flex p-mt-6 p-jc-center p-px-2">
+    <LoginForm @login-done="closeLoginDone"/>
+  </div>
+</template>
+
+<script>
+import LoginForm from '@/components/form/LoginForm'
+import {useRouter} from 'vue-router'
+
+
+export default {
+  name: 'Login',
+  components: {
+    LoginForm
+  },
+  setup() {
+    const router = useRouter()
+    const closeLoginDone = () => {
+      router.push("/")
+    }
+
+    return {
+      closeLoginDone
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
